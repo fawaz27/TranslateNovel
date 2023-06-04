66248c407c7c91e714152442b607c2ab68fa7b4a
fawaz27 <boukarifawaz27@gmail.com>
2023-06-04 01:03:49 +0100

    env correction

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-const url = import.meta.env.VITE_API_URL_DEPLOY;
-
+const url = import.meta.env.VITE_API_APP_URL;
+console.log(url);
 import axios from 'axios';
 const api = axios.create({
     withCredentials: true,
