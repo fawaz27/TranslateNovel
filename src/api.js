3167fd2600bacdeb14998ab854ac9fbf30d00434
fawaz27 <boukarifawaz27@gmail.com>
2023-06-04 01:07:43 +0100

    final coorection

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 const url = process.env.VITE_API_APP_URL;
-console.log(url);
+
 import axios from 'axios';
 const api = axios.create({
     withCredentials: true,
-    baseURL: url
+    baseURL: 'https://trans-novel-api.vercel.app/'
 }) 
 
 export default api;
