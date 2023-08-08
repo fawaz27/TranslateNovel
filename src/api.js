ad70e09506437da9e3d638ebc88ed49616adaff2
Fawaz <75852705+fawaz27@users.noreply.github.com>
2023-08-08 22:14:30 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const url = process.env.VITE_API_APP_URL;
 import axios from 'axios';
 const api = axios.create({
     withCredentials: true,
-    baseURL: 'https://trans-novel-api.vercel.app/'
+    baseURL: 'https://transnovel-api.vercel.app/'
 }) 
 
 export default api;
